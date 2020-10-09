4ba260c124fec615d1fc2fd6921ad54ba83edadc
Shourya <iib2019038@iiita.ac.in>
2020-10-09 19:21:47 +0530

    Implemented Search Filter.

--- app/src/main/java/com/example/opencode20/MainActivity.java
+++ app/src/main/java/com/example/opencode20/MainActivity.java
@@ -42,8 +42,8 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
